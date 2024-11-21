4b0e5660db36ce87ee6e36b561ab1260358c54f5
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2024-11-21 09:25:33 -0400

    Added constraints on ProductRequest

--- product-rest-api-application/src/main/java/org/example/productrestapiapplication/payload/ProductRequest.java
+++ product-rest-api-application/src/main/java/org/example/productrestapiapplication/payload/ProductRequest.java
@@ -1,10 +1,21 @@
 package org.example.productrestapiapplication.payload;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class ProductRequest {
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than 0")
     private double price;
+
+    @NotNull(message = "Quantity is required")
+    @Positive(message = "Quantity must be greater than 0")
     private int quantity;
 }
